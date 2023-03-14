c64a822f7537216b8e17a163ea77e1ab8afefdf8
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-03-14 13:16:26 -0300

    Finalização da introdução

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        files: {
+          'main.css': 'main.less'
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+        },
+        files: {
+          'main.min.css': 'main.less'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'main2.css': 'main.sass'
+        }
+      }
+    },
+    concurrent: {
+      target: ['olaGrunt', 'less', 'tarefaDemorada', 'sass']
+    }
+  })
+
+  grunt.registerTask('tarefaDemorada', function() {
+    const done = this.async();
+    setTimeout(function() {
+      console.log('Olá Grunt');
+      done();
+    }, 3000);
+  })
+
+  grunt.registerTask('olaGrunt', function() {
+    const done = this.async();
+    setTimeout(function() {
+      console.log('Olá Grunt');
+      done();
+    }, 3000);
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['concurrent'])
+}
